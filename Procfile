c1063970d00e6916683e7877f522f7891491ee6b
shandytp <shandytsalasa@gmail.com>
2020-08-30 14:35:15 +0700

    first commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi:app --log-file=-
+web: gunicorn -w 4 -k uvicorn.workers.UvicornWorker pythoncode:app --log-file=-
--- ocr_core.py
+++ ocr_core.py
@@ -5,6 +5,7 @@ def ocr_core(filename):
     """
     This function will handle the core OCR processing of images.
     """
+    pytesseract.pytesseract.tesseract_cmd = r'D:/Tesseract-OCR/tesseract.exe'
     text = pytesseract.image_to_string(Image.open(filename)) # Open image with PIL
 
     return text
