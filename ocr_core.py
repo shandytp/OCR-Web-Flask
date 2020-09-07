b818778403743db190181efceb0d8e3988eafc88
shandytp <shandytsalasa@gmail.com>
2020-09-07 22:33:30 +0700

    insert all preprocessing to ocr_core function

--- ocr_core.py
+++ ocr_core.py
@@ -1,31 +1,35 @@
 from PIL import Image
 import pytesseract
 import cv2
+import numpy as np
 
 def ocr_core(filename):
     """
     This function will handle the core OCR processing of images.
     """
-    pytesseract.pytesseract.tesseract_cmd = r'D:/Tesseract-OCR/tesseract.exe'
-    text = pytesseract.image_to_string(Image.open(filename)) # Open image with PIL
+    # pytesseract.pytesseract.tesseract_cmd = r'D:/Tesseract-OCR/tesseract.exe'
+    # nparray = np.array(filename)
+    # img = Image.fromarray(nparray)
+
+    # Read image with CV2
+    img = cv2.imread(filename)
+
+    # Resize image to 150x150
+    resize = cv2.resize(img, (150, 150))
+
+    # Convert image to grayscale
+    grayscale = cv2.cvtColor(resize, cv2.COLOR_BGR2GRAY)
+
+    # Give image blur to noise image
+    blur = cv2.medianBlur(grayscale, (5,5))
+
+    # Remove noise with Otsu
+    adaptive_thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    # Process OCR
+    text = pytesseract.image_to_string(adaptive_thresh)
 
     return text
 
-def resizeImg(filename):
-    """
-    This function will handle resize image
-    """
-    return cv2.resize(filename, (150, 150))
 
-def grayscaleImg(filename):
-    """
-    This function will process image to grayscale
-    """
-    return cv2.cvtColor(filename, cv2.COLOR_BGR2GRAY)
 
-def adaptive_thresholding(filename):
-    """
-    This function will handle noises in the image and convert it to thresholding
-    """
-    blur = cv2.medianBlur(filename, (5, 5))
-    return cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
